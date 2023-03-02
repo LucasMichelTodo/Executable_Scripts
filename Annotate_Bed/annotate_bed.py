1b12ee3816a1044b46e64127e9d67359faa187d7
LucasMichelTodo <micheltod@gmail.com>
2023-03-02 16:04:32 +0100

    first working version of universal bed annotator

--- Annotate_Bed/annotate_bed.py
+++ Annotate_Bed/annotate_bed.py
@@ -2,22 +2,6 @@ import os
 import pybedtools as pb
 from itertools import chain
 
-def cross_bed_with_gff(cov_file, bed_file, outdir):
-    '''
-    Get coverage from cov_file in regions from bed_file.
-    '''
-    coverage = pb.BedTool(cov_file)
-    bed = pb.BedTool(bed_file)
-    name = pth.Path(cov_file).name.replace('.bdg', '')
-    bedname = pth.Path(bed_file).name.replace('.bed', '')
-    print(f'Calculating {name} coverage over regions in {bedname}.')
-    cov = bed.sort().map(coverage, c = 4, o='mean')
-
-    outname = '_crossed_'.join([name, bedname])+'.tsv'
-    outfile = pth.Path(outdir).joinpath(pth.Path(outname))
-    cov.saveas(outfile)
-    print(f'Generated: {str(outfile)}')
-
 def parse_gff(gff_annot):
     '''
     Parse the annotation of a gff file entry. Return it as a dict.
@@ -39,7 +23,7 @@ gff_cols = ['Gff_Chrom', 'Gff_Source', 'Gff_Feature',
             'Gff_Strand', 'Gff_Frame']
 
 cross = bed.intersect(gff, wao=True)
-annots = [x.fields[ncols_bed+8] for x in cross]
+annots = [x.fields[ncols_b+8] for x in cross]
 annots = [parse_gff(x) if x is not '.' else {} for x in annots]
 
 ## Get all fields present in annotations
@@ -47,18 +31,18 @@ annots = [parse_gff(x) if x is not '.' else {} for x in annots]
 anot_cols = set(chain.from_iterable([list(x.keys()) for x in annots]))
 colnames_g = gff_cols + ['Annot_' + x for x in anot_cols]
 
-with open(outfile, 'w+') as out_file:
-    out_file.write('\t'.join(colnames_b+colnames_g)+'\n')
-    
 
-for x in cross:
-    print(x)
-    annot_field = x.fields[ncols_bed+8]
-    if annot_field != '.':
-        annot = parse_gff(annot_field)
-        for k, v in annot.items():
-            print(f'{k}: {v}')
-    print('-------------------------\n')
+with open(outfile, 'w+') as out_file:
+    colnames = colnames_b + colnames_g + ['Overlap']
+    out_file.write('\t'.join(colnames)+'\n')
+    for x in cross:
+        info = x.fields[ncols_b+8]
+        if info != '.':
+            anot_fields = [parse_gff(info).get(y, '.') for y in anot_cols]
+        else:
+            anot_fields = ['.']*len(anot_cols)
+        line = x.fields[:-2] + anot_fields + [x.fields[-1]]
+        out_file.write('\t'.join(line)+'\n')
 
 
 ### Calls
@@ -68,5 +52,5 @@ os.chdir(wd)
 bedfile = 'example_bed.bed'
 bedfile = './difpeaks_NF54_+cho_rep2_over_NF54_-cho_rep2_w500_s100_pd0.2_mg500_ml1000_difpeaks.bed'
 ref_gff = './PlasmoDB-61_Pfalciparum3D7.gff'
-outfile = 'test_output.tsv'
+outfile = 'test_output_2.tsv'
 
